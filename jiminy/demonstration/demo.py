d2b0adb7e3acf7784feda0e8f1ad24b3fbb6981b
sibeshkar <sibeshkar@users.noreply.github.com>
2019-07-16 11:07:15 +0530

    framebufferpy

--- jiminy/demonstration/demo.py
+++ jiminy/demonstration/demo.py
@@ -12,9 +12,10 @@ class VNCDemonstration(object):
         self.pixel_format = dict()
         self.initialize_from_file(filepath)
         self.initMsg()
+        print(filepath)
 
     def initialize_from_file(self, filepath):
-        with open(sys.argv[1], "rb") as f:
+        with open(filepath, "rb") as f:
             self.demonstration.ParseFromString(f.read())
 
     def initMsg(self):
@@ -36,6 +37,12 @@ class VNCDemonstration(object):
             "greenshift" : self.demonstration.initmsg.PixelFormat.GreenShift,
             "blueshift" : self.demonstration.initmsg.PixelFormat.BlueShift,
         }
+    def listPointerEvents(self):
+        for pointerevent in self.demonstration.pointerevents:
+            print(pointerevent.Mask, pointerevent.X, pointerevent.Y, pointerevent.timestamp)
+    def listFBUpdates(self):
+        for fbupdate in self.demonstration.fbupdates:
+            print(len(fbupdate.rectangles), fbupdate.timestamp)
 
     def printInitMsg(self):
         print("FBHeight:", self.fbheight)
@@ -46,14 +53,41 @@ class VNCDemonstration(object):
         print("DesktopName:", self.desktopname)
         print("PixelFormat", self.pixel_format)
 
+class FBUpdateReader(object):
+    def __init__(self, filepath):
+        self.demonstration = demo_pb2.Demonstration()
+        self.initialize_from_file(filepath)
+
+    def initialize_from_file(self, filepath):
+        with open(filepath, "rb") as f:
+            self.demonstration.ParseFromString(f.read())
+
+        self.fbupdates = [x for x in self.demonstration.fbupdates]
+
+    def __iter__(self):
+        return self
+    
+    def next(self):
+        return self.__next__()
+
+    def __next__(self):
+        yield self.fbupdates
+
+        
+
+
+
+
 
 
 # Main procedure:  Reads the entire address book from a file and prints all
 #   the information inside.
-if len(sys.argv) != 2:
-  print("Usage:", sys.argv[0], "DEMO_FILE")
-  sys.exit(-1)
+# if len(sys.argv) != 2:
+#   print("Usage:", sys.argv[0], "DEMO_FILE")
+#   sys.exit(-1)
 
-vncdemo = VNCDemonstration(sys.argv[1])
+# vncdemo = VNCDemonstration(sys.argv[1])
 
-vncdemo.printInitMsg()
+# vncdemo.printInitMsg()
+# vncdemo.listPointerEvents()
+# vncdemo.listFBUpdates()
